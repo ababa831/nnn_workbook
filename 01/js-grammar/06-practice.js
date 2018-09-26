35f6c3facce1a2dda481df722f3cf3568d7195a8
ababa893 <flvonlineconverter@gmail.com>
2018-09-26 22:25:25 +0900

    Add 01/06 workbook and codes

---
+++ 01/js-grammar/06-practice.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+
+    var radius = 12;  // cm
+    const pi = 3.14;  // circle ratio
+    var area = radius * radius * pi;  // Formula of circle area: (radius)^2 * pi
+    document.write(area);
+})();
---
+++ 01/js-grammar/calc.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+    var x = 10;
+    document.write(x);
+
+    var y = '<p>JavaScript の変数</p>';
+    document.write(y)
+
+    var a = 34;
+    var b = -0.25 * a + 14.1;
+    document.write(b);
+
+    let c = 22;
+    c = 2;
+    document.write('<p>' + c + '</p>');
+
+    // このコードは三角形の面積を求めるプログラムです．
+    var w = 3;  // 三角形の頂点
+    var h = 10;  // 三角形の高さ
+    var area = w * h / 2;  // 三角形の面積の公式「（底辺）×（高さ）÷2」を使って計算し、areaという名前の変数に代入する
+    document.write(area)  // 面積を出力する
+
+})();
